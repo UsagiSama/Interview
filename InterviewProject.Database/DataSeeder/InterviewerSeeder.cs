ee9d9d81f318cee77cc220a02738e0fbb6f34c4a
Герман <templar9@yandex.ru>
2023-11-13 18:12:49 +0700

    Добавлен комментарий о том, как не надо исправлять кейс с некорректным формированием ФИО

--- InterviewProject.Database/DataSeeder/InterviewerSeeder.cs
+++ InterviewProject.Database/DataSeeder/InterviewerSeeder.cs
@@ -11,6 +11,9 @@ namespace InterviewProject.Database.DataSeeder
             {
                 new Interviewer { Id = 1, FirstName = "Иван", LastName = "Иванов", MiddleName = "Иванович" },
                 new Interviewer { Id = 2, FirstName = "Петр", LastName = "Петров", MiddleName = "Петрович" },
+                //Согласно постановке от бизнеса пробел - это корректное значение для поля MiddleName.
+                //В случае, если MiddleName не содержит значимых букв (пустое или только пробелы), то оно не должно отображаться.
+                //Например, в ситуации ниже будет "Smith J.", но не "Smith J.  ."
                 new Interviewer { Id = 3, FirstName = "John", LastName = "Smith", MiddleName = " " },
                 new Interviewer { Id = 4, FirstName = "Hans", LastName = "Zimmer" }
             };
